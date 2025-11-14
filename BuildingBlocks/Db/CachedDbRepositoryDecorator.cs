fee7d1c659d0a8c9aea9b0bba2ad02af1a816a80
PortfolioJankowski <mat.jankowski99@gmail.com>
2025-11-14 12:54:19 +0100

    Implement DbRepository and CachedRepoDecorator

---
+++ BuildingBlocks/Db/CachedDbRepositoryDecorator.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Extensions.Caching.Distributed;
+
+namespace BuildingBlocks.Db
+{
+    public class CachedDbRepositoryDecorator(IDbRepository innerRepository,
+            IDistributedCache cache) : IDbRepository
+    {
+        public byte[] GetDataBlob(Guid id)
+        {
+            var cachedValue = cache.GetString(id.ToString());
+            if (cachedValue != null)
+            {
+                return Convert.FromBase64String(cachedValue);
+            }
+
+            return innerRepository.GetDataBlob(id);
+        }
+
+        public Guid GetFromOutbox()
+        {
+            return innerRepository.GetFromOutbox();
+        }
+
+        public Guid InsertData(byte[] blob)
+        {
+            var recordId = innerRepository.InsertData(blob);
+
+            // 2. Dodaj do cache (Redis) po udanym zapisie do DB/Outbox
+            // Używamy Base64, ponieważ IDistributedCache pracuje najlepiej z ciągami znaków/byte[]
+            // W tym przypadku Convert.ToBase64String(blob) działa jako bezpieczny ciąg znaków.
+            cache.SetString(
+                recordId.ToString(),
+                Convert.ToBase64String(blob),
+                new DistributedCacheEntryOptions
+                {
+                    AbsoluteExpirationRelativeToNow = TimeSpan.FromMinutes(5) // Ustawiamy TTL (Redis)
+                });
+
+            return recordId;
+        }
+    }
+}
---
+++ BuildingBlocks/Db/DbRepository.cs
@@ -0,0 +1,44 @@
+﻿namespace BuildingBlocks.Db
+{
+    public class DbRepository(ApiDbContext dbContext) : IDbRepository
+    {
+        public byte[] GetDataBlob(Guid id)
+        {
+            return dbContext.Blobs.FirstOrDefault(b => b.Id == id)?.Data;
+        }
+
+        public Guid GetFromOutbox()
+        {
+            var record = dbContext.Outboxes.FirstOrDefault(o => o.IsProcessed == false);
+            return record.Id;
+        }
+
+        public Guid InsertData(byte[] blob)
+        {
+            var transaction = dbContext.Database.BeginTransaction();
+            try
+            {
+                var newBlob = new Blob { Data = blob };
+                dbContext.Blobs.Add(newBlob);
+                dbContext.SaveChanges(); // Zapisujemy, aby Entity Framework nadał ID.
+
+                dbContext.Outboxes.Add(new Outbox
+                {
+                    Id = newBlob.Id,
+                    Payload = newBlob.Data, // Na potrzeby testu zostawiamy Payload, inaczej raczej podbierałbym z Blobów
+                    IsProcessed = false
+                });
+
+                dbContext.SaveChanges();
+                transaction.Commit();
+
+                return newBlob.Id;
+            }
+            catch (Exception)
+            {
+                transaction.Rollback();
+                throw; // Rzucamy wyjątek, aby BasicNack został wywołany w konsumencie
+            }
+        }
+    }
+}
