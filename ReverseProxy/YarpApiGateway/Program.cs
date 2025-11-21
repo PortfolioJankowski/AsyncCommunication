c2735149f00b8388f25dc35d93f2e38f3b3f8340
PortfolioJankowski <mat.jankowski99@gmail.com>
2025-11-21 12:51:59 +0100

    add yarp api gateway

--- ReverseProxy/YarpApiGateway/Program.cs
+++ ReverseProxy/YarpApiGateway/Program.cs
@@ -1,5 +1,23 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using Microsoft.AspNetCore.RateLimiting;
 
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddReverseProxy()
+    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
+builder.Services.AddRateLimiter(options =>
+{
+    options.AddFixedWindowLimiter("fixed", config =>
+    {
+        config.PermitLimit = 100;
+        config.Window = TimeSpan.FromMinutes(1);
+        config.QueueProcessingOrder = System.Threading.RateLimiting.QueueProcessingOrder.OldestFirst;
+        config.QueueLimit = 0;
+    });
+});
+
+var app = builder.Build();
+app.UseRateLimiter(); //przed mapowaniem proxy
+app.MapReverseProxy();
 
 app.Run();
